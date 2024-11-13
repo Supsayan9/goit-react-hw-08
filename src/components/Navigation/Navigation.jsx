4f82619683396ec73a07f32ad6feff047bd58056
Supsayan9 <127434945+Supsayan9@users.noreply.github.com>
2024-11-13 14:55:13 +0100

    +

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,11 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import clsx from "clsx";
 import s from "../Navigation/Navigation.module.css";
+
 const Navigation = () => {
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+
   const buildLinkClass = ({ isActive }) =>
     clsx(s.link, isActive && s.activeLink);
 
@@ -10,12 +14,11 @@ const Navigation = () => {
       <NavLink className={buildLinkClass} to="/">
         Home
       </NavLink>
-      <NavLink
-        className={buildLinkClass}
-        to="/contacts"
-      >
-        Contacts
-      </NavLink>
+      {isLoggedIn && (
+        <NavLink className={buildLinkClass} to="/contacts">
+          Contacts
+        </NavLink>
+      )}
     </div>
   );
 };
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -4,56 +4,60 @@ import axios from "axios";
 axios.defaults.baseURL = "https://connections-api.goit.global/";
 
 const setAuthHeader = (token) => {
-    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+const clearAuthHeader = () => {
+  delete axios.defaults.headers.common.Authorization;
 };
 
 export const register = createAsyncThunk(
-    "register",
-    async (credentials, thunkApi) => {
-        try {
-            const { data } = await axios.post("users/signup", credentials);
-            setAuthHeader(data.token);
-            return data;
-        } catch (error) {
-            console.error("Error details:", error.response);
-            return thunkApi.rejectWithValue(error.message);
-        }
+  "auth/register",
+  async (credentials, thunkApi) => {
+    try {
+      const { data } = await axios.post("users/signup", credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      console.error("Error details:", error.response);
+      return thunkApi.rejectWithValue(error.message);
     }
+  }
 );
 
 export const login = createAsyncThunk(
-    "login",
-    async (credentials, thunkApi) => {
-        try {
-            const { data } = await axios.post("users/login", credentials);
-            setAuthHeader(data.token);
-            return data;
-        } catch (error) {
-            return thunkApi.rejectWithValue(error.message);
-        }
+  "auth/login",
+  async (credentials, thunkApi) => {
+    try {
+      const { data } = await axios.post("users/login", credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
     }
+  }
 );
 
-export const logout = createAsyncThunk("logout", async (_, thunkApi) => {
-    try {
-        await axios.post("users/logout");
-        setAuthHeader("");
-    } catch (error) {
-        return thunkApi.rejectWithValue(error.message);
-    }
+export const logout = createAsyncThunk("auth/logout", async (_, thunkApi) => {
+  try {
+    await axios.post("users/logout");
+    clearAuthHeader();
+  } catch (error) {
+    return thunkApi.rejectWithValue(error.message);
+  }
 });
 
-export const refresh = createAsyncThunk("refresh", async (_, thunkApi) => {
-    const savedToken = thunkApi.getState().auth.token;
-    if (!savedToken) {
-        return thunkApi.rejectWithValue("Token doesn`t exist!");
-    }
-    setAuthHeader(savedToken);
-    try {
-        const { data } = await axios.get("users/current");
-        return data;
-    } catch (error) {
-        console.error("Refresh error:", error.response);
-        return thunkApi.rejectWithValue(error.message);
-    }
+export const refresh = createAsyncThunk("auth/refresh", async (_, thunkApi) => {
+  const savedToken = thunkApi.getState().auth.token;
+  if (!savedToken) {
+    return thunkApi.rejectWithValue("Token doesn’t exist!");
+  }
+  setAuthHeader(savedToken);
+  try {
+    const { data } = await axios.get("users/current");
+    return data;
+  } catch (error) {
+    console.error("Refresh error:", error.response);
+    return thunkApi.rejectWithValue(error.message);
+  }
 });
